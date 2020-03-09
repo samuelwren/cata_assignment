44cd3bebadef7745c5565a951bf2f5e504a0b4d6
Samuel Wren <samwren7@hotmail.com>
2020-03-09 19:05:41 +1000

    created the help function4

--- user_upload.php
+++ user_upload.php
@@ -87,4 +87,15 @@
       insert_data($argvs["--file"]);
   }
 
+  // Help Function
+  function help()
+  {
+    echo " --file [csv file name] – this is the name of the CSV to be parsed \n";
+    echo " --create_table – this will cause the MySQL users table to be built (and no further action will be taken) \n";
+    echo " --dry_run – this will be used with the --file directive in case we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered\n";
+    echo " -u – MySQL username \n";
+    echo " -p – MySQL password \n";
+    echo " -h – MySQL host";
+  }
+
 ?>
