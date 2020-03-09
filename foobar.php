afa5219c0b29d426281c9ccd9888d83c515d4662
Samuel Wren <samwren7@hotmail.com>
2020-03-09 20:28:57 +1000

    foobar is now printing out foo for numbers divided by 3 and bar for those divided by 5 and foobar if number is divided by both

--- foobar.php
+++ foobar.php
@@ -3,7 +3,21 @@
   $number = 1;
 
   do {
-    echo "$number \n";
+    $output_string = "";
+    if( $number % 3 == 0 )
+    {
+      $output_string .= "foo";
+    }
+    if( $number % 5 == 0  )
+    {
+      $output_string .= "bar";
+    }
+    if( $number % 5 != 0 && $number % 3 != 0 )
+    {
+      $output_string = $number;
+    }
+
+    echo "$output_string \n";
     $number++;
   } while ($number <= 100);
 
