4dd3bad0078d676bb5fa6831ccf91e708e109173
Samuel Wren <samwren7@hotmail.com>
2020-03-09 19:02:03 +1000

    added in comments

--- user_upload.php
+++ user_upload.php
@@ -1,61 +1,63 @@
 <?php
 
-// Global Variables
-$argvs = array();
-$is_dry_running = FALSE;
-$current_arg = "";
+  // Global Variables
+  $argvs = array();
+  $is_dry_running = FALSE;
+  $current_arg = "";
 
-// Determines which arguments are being called
-// Reads through $argv and stores the argument name as the array position
-foreach( $argv as $item )
-{
+  // Determines which arguments are being called
+  // Reads through $argv and stores the argument name as the array position
+  foreach( $argv as $item )
+  {
 
-  if( strpos($item, '--file' ) !== FALSE )
-  {
-    $current_arg = "--file";
-
-    $file_name = str_replace('--file[', '',$item);
-    $file_name = str_replace(']', '',$file_name);
-
-    $argvs[$current_arg] = $file_name;
-
-  }
-  else if('--create_table' == $item )
-  {
-    $current_arg = "--create_table";
-    $argvs[$current_arg] = TRUE;
-  }
-  else if ('-u' == $item )
-  {
-    $current_arg = "-u";
-    $argvs[$current_arg] = "";
-  }
-  else if ('-p' == $item )
-  {
-    $current_arg = "-p";
-    $argvs[$current_arg] = "";
-  }
-  else if ('-h' == $item )
-  {
-    $current_arg = "-h";
-    $argvs[$current_arg] = "";
-  }
-  else if ('--help' == $item )
-  {
-    $current_arg = "--help";
-    $argvs[$current_arg] = "";
-  }
-  else if( '--dry_run' == $item )
-  {
-    $is_dry_running = TRUE;
-  }
-  else
-  {
-    if( $item != "-" )
+    // Determines which arguement is being passed
+    // Storing the arguemnt as the key in an array
+    if( strpos($item, '--file' ) !== FALSE )
     {
-      $argvs[ $current_arg ] = $item;
+      $current_arg = "--file";
+      // Removes the file name from the input;
+      $file_name = str_replace('--file[', '',$item);
+      $file_name = str_replace(']', '',$file_name);
+      // Saves the arguement
+      $argvs[$current_arg] = $file_name;
+    }
+    else if('--create_table' == $item )
+    {
+      $current_arg = "--create_table";
+      $argvs[$current_arg] = TRUE;
+    }
+    else if ('-u' == $item )
+    {
+      $current_arg = "-u";
+      $argvs[$current_arg] = "";
+    }
+    else if ('-p' == $item )
+    {
+      $current_arg = "-p";
+      $argvs[$current_arg] = "";
+    }
+    else if ('-h' == $item )
+    {
+      $current_arg = "-h";
+      $argvs[$current_arg] = "";
+    }
+    else if ('--help' == $item )
+    {
+      $current_arg = "--help";
+      $argvs[$current_arg] = "";
+    }
+    else if( '--dry_run' == $item )
+    {
+      $is_dry_running = TRUE;
+    }
+    else
+    {
+      // ignores the dash in the arguement and stores the item in the last known arguemnt
+      if( $item != "-" )
+      {
+        $argvs[ $current_arg ] = $item;
+      }
     }
   }
-}
 
 ?>
