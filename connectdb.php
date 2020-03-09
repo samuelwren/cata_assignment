b055ff79856a2ce8c4bb219d02e169a16c012b3f
Samuel Wren <samwren7@hotmail.com>
2020-03-09 19:50:25 +1000

    connected config to the connectdb

--- connectdb.php
+++ connectdb.php
@@ -3,14 +3,17 @@
   // Opens connection to DB
   function OpenConn()
   {
+    //connect config.php
+    include( 'config.php' );
+
     // Database credentials
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
+    $dbhost = $host;
+    $dbuser = $username;
+    $dbpass = $password;
 
     // Opens connection to DB
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Connect failed: %s\n". $conn->error);
-    
+
     return $conn;
   }
 
